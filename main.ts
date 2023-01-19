3f89c3ff8995a287c8614835e3ad00a0fb61b692
Noah062810 <122543656+Noah062810@users.noreply.github.com>
2023-01-19 09:41:40 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . . . # .
+        . . # . .
+        . # . . .
+        . . # . .
+        . . . # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("HI!")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("WOAH!")
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . # . . .
+        . . # . .
+        . . . # .
+        . . # . .
+        . # . . .
+        `)
+})
+basic.showIcon(IconNames.Heart)
